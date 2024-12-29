c082ca306fdedc23de5382351b194d51188279aa
DERMANRAY <rizkydermawan12345@gmail.com>
2024-12-29 22:04:26 +0700

    Add files via upload

---
+++ LIBURAN.js
@@ -0,0 +1,12 @@
+/**
+ * case jika weekend, ucapkan selamat hari libur
+ */
+
+let HariLibur = true //input apakah sedang libur?? .....
+
+if (HariLibur) {
+    console.log ("HAPPY WEEKEND YA!")
+}
+else {
+    console.log ("WAKTUNYA BEKERJA EYYYYY")
+}
